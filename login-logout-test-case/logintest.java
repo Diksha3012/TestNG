376db6e04d673cf5e4da1bf504a1b7e062b97a3a
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-12-28 11:53:26 +0530

    Login-logout-test-cases

---
+++ login-logout-test-case/logintest.java
@@ -0,0 +1,19 @@
+package ui;
+
+import org.testng.annotations.Test;
+
+public class logintest {
+    @Test
+	public  void Logintest() {
+		
+		
+		System.out.println("Login is successful");
+		
+		
+
+	}
+     @Test
+    public void logouttest() {
+    	 System.out.println("Logout is successful");
+    }
+}

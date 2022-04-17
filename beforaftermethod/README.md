f0d208ab99c5c23a32e89e6d8d4fd18446388ad2
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2022-04-17 12:29:14 +0530

    Before and after method annotation

---
+++ beforaftermethod/README.md
@@ -0,0 +1,12 @@
+hey you are logged In
+Lets connect to the database
+Login is successful
+Hey! You lost database connection
+Lets connect to the database
+Logout is successful
+Hey! You lost database connection
+Bye! You are logged out
+PASSED: logouttest
+        This is logout test
+PASSED: Logintest
+        This is login test
---
+++ beforaftermethod/beforeaftermethod.java
@@ -0,0 +1,53 @@
+package ui;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+
+public class beforeafterannotations {
+	
+	@BeforeTest
+	public void logintoapplication()
+	{
+		System.out.println("hey you are logged In");
+	}
+	@AfterTest
+	public void logoutfromapplication()
+	{
+		System.out.println("Bye! You are logged out");
+	}
+	@BeforeMethod
+	public void connectToDB()
+	
+	{
+		System.out.println("Lets connect to the database");
+	}
+    @AfterMethod
+	public void DisconnectFromDB()
+	
+	{
+		System.out.println("Hey! You lost database connection");
+	}
+	
+	
+	@Test(priority=1,description="This is login test")
+	public  void Logintest() {
+		System.out.println("Login is successful");
+	}
+     @Test(priority=2, description="This is logout test")
+    public void logouttest() {
+    	 System.out.println("Logout is successful");
+
+     }
+   
+ 	}
+
+
+
+
+		
+	
+
+
